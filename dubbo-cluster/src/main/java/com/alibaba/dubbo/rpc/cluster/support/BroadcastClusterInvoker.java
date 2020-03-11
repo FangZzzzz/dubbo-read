b7b1e61623908ad077f6a6a0f344f7d083a72464
fufz <fufz@19pay.com.cn>
2020-03-11 14:19:45 +0800

    [feature] Broadcast会循环调用所有invoke，只要有一个失败，在循环调用结束后就会抛出异常，会不会漏掉了异常信息，比如多个失败？

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/BroadcastClusterInvoker.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/support/BroadcastClusterInvoker.java
@@ -47,6 +47,7 @@ public class BroadcastClusterInvoker<T> extends AbstractClusterInvoker<T> {
         RpcContext.getContext().setInvokers((List) invokers);
         RpcException exception = null;
         Result result = null;
+        // 遍历 Invoker 列表，逐个调用
         for (Invoker<T> invoker : invokers) {
             try {
                 result = invoker.invoke(invocation);
