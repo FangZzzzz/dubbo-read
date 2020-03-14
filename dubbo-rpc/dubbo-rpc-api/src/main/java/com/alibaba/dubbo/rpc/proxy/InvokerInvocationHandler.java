4f6a7035f8c28d4bce25da6de11df6de70282a79
fufz <fufz@19pay.com.cn>
2020-03-14 16:27:04 +0800

    [feature] 看了一下消费方调用逻辑

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/InvokerInvocationHandler.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/proxy/InvokerInvocationHandler.java
@@ -22,6 +22,38 @@ import com.alibaba.dubbo.rpc.RpcInvocation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+
+/**
+ *
+ * 字节码生成的代理类
+ * public class proxy0 implements ClassGenerator.DC, EchoService, DemoService {
+ *     // 方法数组
+ *     public static Method[] methods;
+ *     private InvocationHandler handler;
+ *
+ *     public proxy0(InvocationHandler invocationHandler) {
+ *         this.handler = invocationHandler;
+ *     }
+ *
+ *     public proxy0() {
+ *     }
+ *
+ *     public String sayHello(String string) {
+ *         // 将参数存储到 Object 数组中
+ *         Object[] arrobject = new Object[]{string};
+ *         // 调用 InvocationHandler 实现类的 invoke 方法得到调用结果
+ *         Object object = this.handler.invoke(this, methods[0], arrobject);
+ *         // 返回调用结果
+ *         return (String)object;
+ *     }
+ *
+ *     public Object $echo(Object object) {
+ *         Object[] arrobject = new Object[]{object};
+ *         Object object2 = this.handler.invoke(this, methods[1], arrobject);
+ *         return object2;
+ *     }
+ */
+
 /**
  * InvokerHandler
  */
@@ -37,9 +69,11 @@ public class InvokerInvocationHandler implements InvocationHandler {
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         String methodName = method.getName();
         Class<?>[] parameterTypes = method.getParameterTypes();
+        // 拦截定义在 Object 类中的方法（未被子类重写），比如 wait/notify
         if (method.getDeclaringClass() == Object.class) {
             return method.invoke(invoker, args);
         }
+        // 如果 toString、hashCode 和 equals 等方法被子类重写了，这里也直接调用
         if ("toString".equals(methodName) && parameterTypes.length == 0) {
             return invoker.toString();
         }
@@ -49,6 +83,7 @@ public class InvokerInvocationHandler implements InvocationHandler {
         if ("equals".equals(methodName) && parameterTypes.length == 1) {
             return invoker.equals(args[0]);
         }
+        // 将 method 和 args 封装到 RpcInvocation 中，并执行后续的调用
         return invoker.invoke(new RpcInvocation(method, args)).recreate();
     }
 
